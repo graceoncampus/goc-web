5dd0fd94fb27dfff771a61bfaa00eb75f7404e95
Georgie Mathews <mathews.georgie@gmail.com>
2018-04-18 12:31:04 -0700

    fixed rides

--- devscripts/delete-rides.js
+++ devscripts/delete-rides.js
@@ -2,14 +2,16 @@ var CronJob = require('cron').CronJob;
 var firebaseDB = require('../build/firebase.js').firebaseDB;
 var ridesRef = firebaseDB.ref("rides");
 var ridesSignupRef = firebaseDB.ref("ridesSignup");
+const deleteRides = async () => {
+  await ridesRef.remove();
+  await ridesSignupRef.remove();
+}
 
-var job = new CronJob({
-  cronTime: '00 00 24 * * SUN',
-  onTick: function() {
-    ridesRef.remove();
-    ridesSignupRef.remove();
-  },
-  start: false,
-  timeZone: 'America/Los_Angeles'
-});
+var job = new CronJob('00 00 24 * * SUN', deleteRides, function () {
+  console.log("done deleting rides!");
+},
+false, /* Start the job right now */
+'America/Los_Angeles' /* Time zone of this job. */
+);
+
 job.start();
