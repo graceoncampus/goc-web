7ef8d7d0e4451c1142984391961b34a440e1107b
Georgie Mathews <mathews.georgie@gmail.com>
2018-05-03 20:19:17 -0700

    calendar

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -3,7 +3,9 @@ import admin from 'firebase-admin';
 import _ from 'lodash';
 
 export const getCalendar = async(req, res) => {
+    let data = await admin.database().ref('/calendar/events').once('value');
     res.render('calendar.ejs', {
+        events: data.val(),
         title: 'Calendar'
     });
 };
