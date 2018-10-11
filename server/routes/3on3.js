5c35ca191a99bd6f02a2e269f08ec47faa4ee09a
Georgie Mathews <mathews.georgie@gmail.com>
2018-10-11 16:09:01 -0700

    working

--- server/routes/3on3.js
+++ server/routes/3on3.js
@@ -1,27 +1,17 @@
-import fs from "fs";
-import async from "async";
-import crypto from "crypto";
-import _ from "lodash";
-import common from "../lib";
 import admin from "firebase-admin";
-import moment from "moment";
 export const get3On3 = (req, res) => res.render("3on3.ejs");
 export const post3On3 = async (req, res) => {
     const { name, email } = req.body;
     const ref = admin.database().ref("3on3");
-    ref
+    const snapshot = await ref
         .orderByChild("email")
         .equalTo(email)
-        .on("value", async snapshot => {
-            if (snapshot.val()) {
-                res.json("Already signed up");
-            } else {
-                try {
-                    await ref.push({ name, email });
-                    res.json("success");
-                } catch (err) {
-                    res.json(err.message);
-                }
-            }
-        });
+        .once("value");
+    if (snapshot.val()) return res.json("Already signed up");
+    try {
+        await ref.push({ name, email });
+        return res.json("success");
+    } catch (err) {
+        return res.json(err.message);
+    }
 };
