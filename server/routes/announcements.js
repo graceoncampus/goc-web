a7bbba8c82498001ca10e567ff75d4bc4de17359
joycekho <joyceh122@gmail.com>
2018-11-15 20:14:26 -0800

    switched announcements to firebase

--- server/routes/announcements.js
+++ server/routes/announcements.js
@@ -1,21 +1,24 @@
 import moment from 'moment';
-import admin from 'firebase-admin';
+import admin from 'firebase-admin'
 import _ from 'lodash';
 import { replaceURLsWithLinks } from '../lib';
 
 export const getAnnouncements = async(req, res) => {
-    let posts = await admin.database().ref('/posts').once('value');
-    posts = _.values(posts.val()).reverse()
+    let posts = admin.firestore().collection('announcements').get().then((snapshot) => {
     const finalPosts = []
-    for (let post of posts) {
-        var formattedPost = replaceURLsWithLinks(post.Post)
-        formattedPost = formattedPost.replace(/\n/g, "<br/>");
-        post.date = moment.unix(post.Time).fromNow();
-        post.Post = formattedPost
-        finalPosts.push(post)
-    }
+    snapshot.forEach((doc) => {
+      let post = doc.data()
+      var formattedPost = replaceURLsWithLinks(post.post)
+      formattedPost = formattedPost.replace(/\n/g, "<br/>");
+      post.Post = formattedPost
+      finalPosts.push(post)
+    });
     res.render('announcements.ejs', {
         title: 'Announcements',
         posts: finalPosts
     });
+  })
+  .catch((err) => {
+    console.log('Error getting documents', err);
+  });
 };
