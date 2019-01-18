7148d281e117625f9dc0a535ae6e9b0c81e5093b
Georgie Mathews <mathews.georgie@gmail.com>
2019-01-17 21:20:16 -0800

    working array push

--- server/firebase.js
+++ server/firebase.js
@@ -1,9 +1,21 @@
-import * as admin from 'firebase-admin';
-import config from './config/firebase.json';
+import * as admin from "firebase-admin";
+import firebase from "firebase";
+require("firebase/firestore");
+import config from "./config/firebase.json";
 admin.initializeApp({
-    credential: admin.credential.cert(config),
-    databaseURL: process.env.databaseURL
-})
+  credential: admin.credential.cert(config),
+  databaseURL: process.env.databaseURL
+});
+var config2 = {
+  apiKey: "AIzaSyC5FN_4EfqAcIoPOOVzTXeze-afOVS_YZo",
+  authDomain: "goc-app-6efe2.firebaseapp.com",
+  databaseURL: "https://goc-app-6efe2.firebaseio.com",
+  projectId: "goc-app-6efe2",
+  storageBucket: "goc-app-6efe2.appspot.com",
+  messagingSenderId: "44635103582"
+};
+firebase.initializeApp(config2);
 
 export default admin;
 export const firestoreDB = admin.firestore();
+export const firestoreDB2 = firebase.firestore();
