36130dcb8e5306a2fa33d799993273c383fb368a
janexn1013 <37201809+janexn1013@users.noreply.github.com>
2019-03-11 02:13:30 -0700

    Can now enroll & unenroll into classes

--- server/firebase.js
+++ server/firebase.js
@@ -1,10 +1,21 @@
 import * as admin from 'firebase-admin';
 import config from './config/firebase.json';
+import firebase from 'firebase'
+require("firebase/firestore");
 admin.initializeApp({
     credential: admin.credential.cert(config),
     databaseURL: process.env.databaseURL
 })
+var config2 = {
+  apiKey: "AIzaSyC5FN_4EfqAcIoPOOVzTXeze-afOVS_YZo",
+  authDomain: "goc-app-6efe2.firebaseapp.com",
+  databaseURL: "https://goc-app-6efe2.firebaseio.com",
+  projectId: "goc-app-6efe2",
+  storageBucket: "goc-app-6efe2.appspot.com",
+  messagingSenderId: "44635103582"
+};
+firebase.initializeApp(config2);
 
 export const firebaseDB = admin.database();
-export const firestoreDB = admin.firestore();
+export const firestoreDB = firebase.firestore();
 export default admin;
