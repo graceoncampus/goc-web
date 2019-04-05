83445b9baee0de42e08de0f8db390ad662d8e59d
joycekho <joyceh122@gmail.com>
2019-04-04 23:25:43 -0700

    changed announcements timestamp

--- server/routes/announcements.js
+++ server/routes/announcements.js
@@ -3,6 +3,20 @@ import { firestoreDB } from '../firebase'
 import _ from 'lodash';
 import { replaceURLsWithLinks } from '../lib';
 
+const times = [
+  60000, // minute
+  3600000, // hour
+  86400000, // day
+  604800000, // week
+  2629800000, // month
+  31557600000, // year
+];
+
+const past = (value, units) => (`${value} ${units} ago`);
+
+past.f = 'just now';
+
+const singleValue = time => (time === 'hour' ? 'an' : 'a');
 export const getAnnouncements = (req, res) => {
     firestoreDB.collection('announcements').get().then((snapshot) => {
     const finalPosts = []
@@ -11,7 +25,7 @@ export const getAnnouncements = (req, res) => {
       var formattedPost = replaceURLsWithLinks(post.post)
       formattedPost = formattedPost.replace(/\n/g, "<br/>");
       post.Post = formattedPost
-      post.date = moment.utc(post.date.toDate())
+      post.date = getRelativeTime(post.date.toDate())
       finalPosts.push(post)
     });
     res.render('announcements.ejs', {
@@ -22,4 +36,46 @@ export const getAnnouncements = (req, res) => {
   .catch((err) => {
     console.log('Error getting documents', err);
   });
+};
+
+export const getRelativeTime = (time) => {
+  const now = new Date();
+  const diff = now.getTime() - time.getTime();
+  let i = 0,
+    units,
+    value;
+  const iMax = times.length;
+  for (; i < iMax; i += 1) {
+    value = times[i];
+    const prev = times[i - 1];
+    if (diff < value) {
+      switch (i) {
+        case 0:
+          return past.f;
+        case 1:
+          units = 'minute';
+          break;
+        case 2:
+          units = 'hour';
+          break;
+        case 3:
+          units = 'day';
+          break;
+        case 4:
+          units = 'week';
+          break;
+        case 5:
+          units = 'month';
+          break;
+        default:
+          break;
+      }
+      value = Math.floor(diff / prev);
+      break;
+    } else if (i === 5) {
+      units = 'year';
+      value = Math.floor(diff / times[i]);
+    }
+  }
+  return value === 1 ? past(singleValue(value), units) : past(value, `${units}s`);
 };
