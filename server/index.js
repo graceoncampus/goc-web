1cd0be122440ffa0531ee40b63da73735f326ec0
Isaac Li <isaac.li@ucla.edu>
2022-08-14 19:35:45 -0700

    add newline sanity check

--- server/index.js
+++ server/index.js
@@ -10,6 +10,7 @@ import cloudinary from 'cloudinary';
 import routes from './routes';
 import { mailgun } from './lib';
 
+
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
